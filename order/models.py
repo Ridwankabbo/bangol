4b9cd55620f5d4a009a0b73dadcd497d4e1cfb63
Ridwankabbo <ridwanislamkabbo@gmail.com>
2026-02-03 20:06:40 +0600

    Made some changes into model

--- order/models.py
+++ order/models.py
@@ -7,11 +7,11 @@ from products.models import Product
 class Order(models.Model):
     class OrderStatus(models.TextChoices):
         PENDING = 'PND','Pending'
-        DELIVERD = 'DEL', 'Deliverd'
+        PAID = 'PAID', 'Paid'
         CANCLED = 'CND', 'Cancled'
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='order')
-    order_number = models.IntegerField(unique=True)
-    status = models.TextField(choices=OrderStatus.choices)
+    order_number = models.IntegerField(unique=True, editable=False)
+    status = models.TextField(choices=OrderStatus.choices, default=OrderStatus.PENDING)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     
@@ -19,6 +19,7 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_item')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
     quantity = models.PositiveIntegerField(null=True,blank=True)
+    price_at_purchase = models.DecimalField(max_digits=12, decimal_places=2) 
     updated_at = models.DateTimeField(auto_now_add=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
